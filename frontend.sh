3ad60377922d533d261334eefa916a2e2a1d6788
rabbanishaik <rabbanishaik2k18@gmail.com>
2025-07-19 11:32:18 +0530

    backend

---
+++ frontend.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/expense-logs"
+LOGS_FILE=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
+LOG_FILE_NAME="$LOGS_FOLDER/$LOGS_FILE-$TIMESTAMP"
+
+
+
+VALIDATE(){
+
+    if [ $1 -ne 0 ]
+        then
+        echo -e "$2 ...$R failue"
+        exit 1
+        else
+        echo -e "$2 ... $G success"
+fi
+}
+
+echo "script installed date: $TIMESTAMP" &>>$LOG_FILE_NAME
+
+CHECK_ROOT(){
+if [ $USERID -ne 0 ]
+then 
+    echo "Error: you must have super prvillages"
+    exit 1
+fi
+}
+
+CHECK_ROOT
+
+dnf install nginx -y 
+VALIDATE $? "install nginx"
+
+systemctl enable nginx
+VALIDATE $? "enable nginx"
+
+systemctl start nginx
+VALIDATE $? "start nginx"
+
+rm -rf /usr/share/nginx/html/*
+VALIDATE $? "remove exists files"
+
+cd /usr/share/nginx/html
+
+unzip /tmp/frontend.zip
+VALIDATE $? "unzip the file"
+
+cp /home/ec-user/shell-script/expense.conf /etc/nginx/default.d/expense.conf
+VALIDATE $? "add config file"
+
+
+systemctl restart nginx
+VALIDATE $? "Restrat the server"
